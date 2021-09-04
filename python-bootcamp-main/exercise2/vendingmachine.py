e5f78c017aa271209f50fb245cf19ca9db46dc41
megannguyen68 <megannguyen6898@gmail.com>
2021-09-05 02:20:36 +1000

    vending machine

--- python-bootcamp-main/exercise2/vendingmachine.py
+++ python-bootcamp-main/exercise2/vendingmachine.py
@@ -1,3 +1,4 @@
+from threading import TIMEOUT_MAX
 from typing import Dict, Any, Union
 
 amount = int(input('Enter the amount paid:'))
@@ -8,60 +9,104 @@ num_coins = [5, 5, 5, 5, 5]
 coins_4_change = [0, 0, 0, 0, 0]
 
 num_items = [10, 10, 10, 10, 10, 10, 10, 10, 10]
+num_items_restocked = [10, 10, 10, 10, 10, 10, 10, 10, 10]
+num_coins_restocked = [5, 5, 5, 5, 5]
 selections = {'Water': 75, 'Coke': 120, 'Diet Coke': 120, 'Iced tea': 100, 'Swiss Chocolate': 150, 'Candy': 95, 'Chips': 110, 'Bubble Gum': 50, 'Turkish Delight': 120}
-transaction = []
+transactions = []
+ids = []
+coin_nb = []
+
+items = [] #store price of item purchased
+item_names = [] #store names of items purchased
 
 class vending_machine:
-    def show_menu():
+    def __init__(self, purchase_ID, secret_PIN, amount):
+        self.purchase_ID = purchase_ID
+        self.secret_PIN = 00
+        self.digits_purchase_ID = [int(d) for d in str(self.purchase_ID)]
+        self.total_amount_to_pay = 0
+        self.amount = amount
+        self.change_amount = self.amount - self.total_amount_to_pay
+    def show_menu(self):
         for a, b in selections.items():
             id = list(selections.keys()).index(a) + 1
             print(str(id), ":", a, " costs ", b, "cents")
+            ids.append(id)
+    def make_selection(self, purchase_ID):
+        if self.purchase_ID == self.secrete_PIN:
+            self.print_confidential_info()
+        else:
+            for i in range(len(self.digits_purchase_ID)):
+                digit = self.digits_purchase_ID[i]
+                if digit > len(ids) or self.purchase_ID <= 0:
+                    self.error_message()
+                else:
+                    ids = set(ids)
+                    if digit in ids:
+                        item_chosen = list(selections.values())[digit-1]
+                        item_name = list(selections.keys())[digit-1]
+                        items.append(item_chosen)
+                        item_names.append(item_name)
+                        self.total_amount_to_pay = 0
+                        for j in range(len(items)):
+                            self.total_amount_to_pay = self.total_amount_to_pay + items[j]
+                num_items[digit-1] = num_items[digit-1] - 1
+            print("You gotta pay: " + str(self.total_amount_to_pay))
+            return self.total_amount_to_pay
 
-    def make_selection(purchase_ID):
-        while purchase_ID != 0:
-            digit = purchase_ID % 10
-            total_amount += selections.values[digit-1]
-            purchase_ID //= 10
-            return total_amount
-        print("The total amount to pay:" + total_amount)
-        for i in range(purchase_ID):
-            if i = showmenu():  # if the digit in item ID matches the items, we subtract the selection from aval items
-                num_items[i-1] -= len(i)
-        transaction.append(purchase_ID)
-
-    def return_change():
-        coin_nb = []
-        print("Your change is:" + change_amount)
-        while change_amount > 0:
+    total_amount_to_pay_per_transaction = make_selection(purchase_ID)
+    transactions.append(total_amount_to_pay_per_transaction)
+    
+    def error_message(self):
+        print("Error!")
+    def return_change(self):
+        print("Your change is: " + str(self.change_amount))
+        if self.change_amount < 0:
+            print("Please deposit more!")
+        else:
             for i in range(len(change_denom)):
-                coin_nb_i = int(change_amount/change_denom[i])
-                coin_nb.append(coin_nb_i)
-                if coin_nb[i] < num_coins[i]:
-                    coins_4_change += coin_nb[i]
-                        if coins_4_change > 0: #only when vm has enough coins for change
-                            change_amount = change_amount - (coin_nb[i] * change_denom[i])
-                            print(change_denom[i] + "cents : " + coin_nb[i])
-                        else:
-                            print("Your selection is not valid")
+                coin_nb_for_denom = int(change_amount/change_denom[i])
+                coin_nb.append(coin_nb_for_denom)
+                if coin_nb[i] <= num_coins[i]:
+                    change_amount = change_amount - change_denom[i] * coin_nb[i]
+                    coins_4_change[i] = coins_4_change[i] + coin_nb[i]
+                    num_coins[i] = num_coins[i] - coin_nb[i]
+                    print("Please take " + str(coins_4_change[i]) + " of " + str(change_denom[i]))
                 else:
                     coin_nb[i] = 0 #if the coins needed for change > coins available, not use that denom at all
+                    print("We don't have enough " + str(change_denom[i]) + " cents for you!")
+        print("End of transaction!")
 
-    def get_money(make_selection):
-        change = amount - total_amount
-        print('your  change is : ', change)
-        return change
-
+    def sales_each_item():
+        #find frequency of items chosen
+        sales_per_item = {}
+        # iterating over the list
+        for item in item_names:
+        # checking the element in dictionary
+            if item in sales_per_item:
+                # incrementing the count
+                sales_per_item[item] += 1
+            else:
+                # initializing the count
+                sales_per_item[item] = 1
+        #find individual sales for each item 
+        common_set = set(sales_per_item.keys()).intersection(set(selections.keys()))
+        common_set = list(common_set)
+        sales_each_item = {}
+        for selection in selections:
+            if selection in common_set:
+                sales_each_item[selection] = selections[selection]
+        for key, value in sales_each_item.items():
+            sales_each_item[key] = value * sales_per_item[key]
     def print_confidential_info():
-        for i in range(len(num_items)):
-            num_selections += purchase_ID % 10
-            while num_selections >= 99:
-                secret_code = int(input("Where is secret PIN code?"))
-                if secret_code == secret_PIN:
-                    print(num_selections)
-                    total_income += num_items[i] * selections.values[i]
-                    print("We earned" + total_income)
-                else:
-                    print("Sorry under maintenance")
+        print("The number of items after restock: ") 
+        for index, num_item in enumerate(num_items):
+            num_items[index] = num_items[index] + num_items_restocked[index]
+            print(list(selections.keys())[index] + " : " + str(num_items[index]))
+        print("The number of coins after restock: ") 
+        for indexx, num_coin in enumerate(num_coins):
+            num_coins[indexx] = num_coins[indexx] + num_coins_restocked[indexx]
+            print(str(change_denom[indexx]) + " : " + str(num_items[indexx]))
 
     def monitor():
         for i in range(len(num_items)):
